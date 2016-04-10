f0d535c57957b0a42d1d01a4c0fad51d4916dda8
bluestraenje <bluestraenje@gmail.com>
2016-04-10 14:58:18 +0800

    commit changes

--- InteriorDesignApplication/InteriorDesignApplication/MainWindow.xaml.cs
+++ InteriorDesignApplication/InteriorDesignApplication/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +14,52 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+using System.Data.Entity;
 
 namespace InteriorDesignApplication
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
         }
+        private readonly ManagerDBContext _context = new ManagerDBContext();
+
+        private IEnumerable<Customer> _customers;
+        public IEnumerable<Customer> Customers
+        {
+            get
+            {
+                return new ObservableCollection<Customer>(_context.Customers.Include(d=>d.Dependents));
+            }
+            set
+            {
+                _customers = value;
+                OnPropertyChanged("Customers");
+            }
+        }
+
+        private IEnumerable<Dependent> _dependents;
+        public IEnumerable<Dependent> Dependents { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;        
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }  
+
+
+
     }
+
+    
+
 }
