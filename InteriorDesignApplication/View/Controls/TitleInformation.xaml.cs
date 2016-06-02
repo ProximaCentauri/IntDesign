7c068ab42f844f15bd487caacde187214b6a64fb
Vanessa Mae Orog <vanessamaeorog@Jullans-MacBook-Air.local>
2016-06-03 00:13:41 +0800

    added titles info panel

---
+++ InteriorDesignApplication/View/Controls/TitleInformation.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using View;
+using Model.Controls;
+using ViewModel;
+using Model;
+using Microsoft.Win32;
+
+namespace View.Controls
+{
+    /// <summary>
+    /// Interaction logic for TitleInformation.xaml
+    /// </summary>
+    public partial class TitleInformation : UserControl
+    {
+        public TitleInformation()
+        {
+            InitializeComponent();
+        }
+
+        private void browseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Image files (*.png;*.jpeg)|*.png;*.jpeg|Pdf files (*.pdf)|*.pdf|All files (*.*)|*.*";
+            Nullable<bool> result = dlg.ShowDialog();
+            if (!String.IsNullOrEmpty(dlg.FileName))
+            {
+                
+            }
+        }
+    }
+}
